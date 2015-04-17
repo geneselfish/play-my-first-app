004574c58d3173ecaa1afdfda999fbf5b9e2222f
k-oga <gene_selfish_k@yahoo.co.jp>
2015-04-17 23:25:10 +0900

    http://www.atmarkit.co.jp/ait/articles/1307/29/news026.html の途中。

---
+++ app/common.scala
@@ -0,0 +1,6 @@
+import play.api._
+object Global extends GlobalSettings {
+  override def onStart(app: Application) {
+    Logger.info("Application has started for %s mode.".format(app.mode))
+  }
+}
